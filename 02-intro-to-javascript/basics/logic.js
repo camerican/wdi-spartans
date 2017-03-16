9d999692f3322fb5eda9dd88ed2f08d55c98b80e
Isaac Kang <isaacjkang@gmail.com>
2017-03-16 11:59:16 -0400

    Add new files to catch up

---
+++ 02-intro-to-javascript/basics/logic.js
@@ -0,0 +1,24 @@
+// if(5 > 10) {
+//   console.log("5 is less than 10")
+// } else {
+//   console.log("5 is not less than 10")
+// }
+
+// var number = 1000
+//
+// if(number < 10) {
+//   alert("this is less than 10")
+// } else if(number < 0) {
+//   alert("this is " + number + " this is greater than 10")
+// } else {
+//   alert("everything is awesome")
+// }
+
+// name function
+function complicatedAdd(number1, number2) {
+  console.log(number1 + number2 * 1000)
+}
+
+// invoke function
+complicatedAdd(5, 6)
+complicatedAdd(10, 7)
---
+++ 02-intro-to-javascript/basics/main.js
@@ -0,0 +1,36 @@
+// creating a variable of names
+var listOfNames = [
+  "Orlando",
+  "Isaac",
+  "Fizal"
+]
+
+// create a car object
+var car = {
+  make: "Toyota",
+  model: "Prius"
+}
+
+// set two numbers
+var number1 = 50
+var number2 = 2000
+
+// set two strings
+var firstName = "orlando"
+var lastName = "caraballo"
+
+
+// create person object
+var person = {
+  first_name: "orlando",
+  last_name: lastName,
+  birthday: "12/01/1983",
+  interests: [
+    "movies",
+    "gaming",
+    "baking"
+  ]
+}
+
+// output person to the console
+console.log(person)
