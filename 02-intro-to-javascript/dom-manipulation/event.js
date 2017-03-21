45f790bfaa7cf1fef5a7c18d7cf6b2bc663e8551
orlandocaraballo <orlando.caraballo@nycda.com>
2017-03-21 08:32:27 -0400

    added new event.js

---
+++ 02-intro-to-javascript/dom-manipulation/event.js
@@ -0,0 +1,13 @@
+// var paragraph = document.getElementById("second")
+//
+// paragraph.addEventListener("mouseenter", function(){
+//   paragraph.style.color = "blue"
+// })
+//
+// paragraph.addEventListener("mouseleave", function(){
+//   paragraph.style.color = "red"
+// })
+
+setTimeout(function(){
+    alert("hello")
+}, 2000)
