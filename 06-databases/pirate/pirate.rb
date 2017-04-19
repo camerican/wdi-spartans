5be1ae5815ae2a6a39ef240bad36b9e5953e1912
orlandocaraballo <orlando.caraballo@nycda.com>
2017-04-19 15:10:04 -0400

    Added flash messages

--- 06-databases/pirate/pirate.rb
+++ 06-databases/pirate/pirate.rb
@@ -2,6 +2,10 @@ require "sinatra"
 require "sinatra/activerecord"
 require "./models"
 
+# this is needed for sinatra flash
+require "bundler/setup"
+require "sinatra/flash"
+
 set :database, "sqlite3:pirate.sqlite3"
 set :sessions, true
 
@@ -47,14 +51,18 @@ end
 post "/sign-in" do
   # puts params[:username]
   # puts params[:password]
-
   @pirate = Pirate.where(name: params[:name]).first
 
   if @pirate.password == params[:password]
     session[:pirate_id] = @pirate.id
+
+    flash[:notice] = "You have been signed in"
+
     redirect "/"
   else
-    redirect "/password-not-found"
+    flash[:notice] = "Your name and password do not match"
+
+    redirect "/sign-in"
   end
 end
 
@@ -67,6 +75,14 @@ get "/profile" do
   @pirate = Pirate.find(session[:pirate_id])
 
   erb :profile
+end
+
+# shortcut method for loading users
+# you can use this in every sintra route
+def current_user
+  if session[:pirate_id]
+    Pirate.find(session[:pirate_id])
+  end
 end
 
 # get "/account" do
