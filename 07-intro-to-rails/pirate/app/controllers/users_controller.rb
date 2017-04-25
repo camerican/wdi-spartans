66909940b284533bc924cda8e0e868189a16878e
orlandocaraballo <orlando.caraballo@nycda.com>
2017-04-25 16:51:53 -0400

    Moar code

--- 07-intro-to-rails/pirate/app/controllers/users_controller.rb
+++ 07-intro-to-rails/pirate/app/controllers/users_controller.rb
@@ -1,11 +1,49 @@
 class UsersController < ApplicationController
   # corresponds to get "/users" => "users#index"
+  # this is an action
   def index
     # loads all users in the database
     @users = User.all
 
     # you can have more than one instance variable
     @colors = ["red", "green"]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.fname = params[:user][:fname]
+    @user.lname = params[:user][:lname]
+
+    if @user.save
+      flash[:notice] = "The user was updated"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "The user was not updated"
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  def create
+    @user = User.new(
+      fname: params[:user][:fname],
+      lname: params[:user][:lname]
+    )
+
+    if @user.save
+      flash[:notice] = "The user was created"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "The user was not created"
+      redirect_to new_user_path
+    end
   end
 
   # corresponds to get "/users/:id" => "users#show"
