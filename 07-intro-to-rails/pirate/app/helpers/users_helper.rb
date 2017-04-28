3e1f5b13ff1868ff2a1d1065e45f22aeaa8b1d00
Cam Crews <camcrews@gmail.com>
2017-04-28 14:39:47 -0400

    View Helpers Examples added

--- 07-intro-to-rails/pirate/app/helpers/users_helper.rb
+++ 07-intro-to-rails/pirate/app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def talk_like_a_pirate
+    ["arrrrrr","ahoy matey","I like the water"].sample
+  end
+
+  def user_div user
+    %Q{
+      <div class="user" id="#{user.id}">
+        #{user.fname}
+      </div>
+    }.html_safe
+  end
+
+  def headline msg
+    content_tag :h2, msg
+  end
 end
